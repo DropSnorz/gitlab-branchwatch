ae8514af3304bb4132632fdcdd81b405343363f7
Arthur POIRET <dropsnorz@gmail.com>
2019-10-21 23:37:12 +0200

    dev: add argparser

--- branchwatch.py
+++ branchwatch.py
@@ -1,15 +1,30 @@
-import gitlab
+import argparse
+import os
 import json
 import requests
+import gitlab
 
 config = {}
-config["gitlab_url"] = "https://gitlab.com"
-config["gitlab_token"] = "secret"
-config["gitlab_group"] = 6337446
-config["webhook_url"] = "secret"
 
 def main():
-    report = generate_branch_report();
+
+    parser = argparse.ArgumentParser(description='Generate gitlab branch report')
+    parser.add_argument('--url', default='https://gitlab.com',
+                    help='Gitlab url (default: https://gitlab.com)')
+    parser.add_argument('--group', type=int, required=True,
+                    help='Gitlab group ID')
+    parser.add_argument('--token',
+                    help='Gitlab private token or $BW_GITLAB_TOKEN')
+    parser.add_argument('--webhook',
+                    help='Webhook url or $BW_WEBHOOK')
+    args = parser.parse_args()
+
+    config["gitlab_url"] = args.url
+    config["gitlab_group"] = args.group
+    config["gitlab_token"] = args.token or os.getenv('BW_GITLAB_TOKEN')
+    config["webhook_url"] = args.webhook or os.getenv('BW_WEBHOOK')
+
+    report = generate_branch_report()
     send_report(report)
 
 
@@ -20,24 +35,26 @@ def generate_branch_report():
         "untrackedBranches": [],
     }
     gl = gitlab.Gitlab(config['gitlab_url'], private_token=config['gitlab_token'])
-
     group = gl.groups.get(config["gitlab_group"])
-    group_merge_requests = group.mergerequests.list(state='opened')
 
+    # Retrieve opened merge requests
+    group_merge_requests = group.mergerequests.list(state='opened')
     for mr in group_merge_requests:
         message = "* %s `%s` -> `%s` : %s" % (mr.title, mr.source_branch, mr.target_branch, mr.web_url)
         if mr.work_in_progress:
-            report["wipBranches"].append(message);
+            report["wipBranches"].append(message)
         else:
-            report["pendingReviewBranches"].append(message);
+            report["pendingReviewBranches"].append(message)
 
-
+    # Iterate over project to find active branches
     for groupProject in group.projects.list():
         project = gl.projects.get(groupProject.id)
         for branch in project.branches.list():
+            # Exclude master branch, protected branches and merged branches
             if branch.name == "master" or branch.protected or branch.merged:
                 continue
 
+            # Find associated merge request (if any)
             mr_opened = False
             for mr in group_merge_requests:
                 if mr.project_id == project.id and mr.source_branch == branch.name:
@@ -46,7 +63,7 @@ def generate_branch_report():
             
             if not mr_opened:
                 message = "* `%s` on %s (last commit by %s)" % (branch.name, project.name, branch.commit["author_name"])
-                report["untrackedBranches"].append(message);
+                report["untrackedBranches"].append(message)
 
     return report
 
@@ -71,7 +88,7 @@ def send_report(report):
             'Request to slack returned an error %s, the response is:\n%s'
             % (response.status_code, response.text)
         )
-    
+
 
 if __name__ == "__main__":
     # execute only if run as a script
